5552e60ad3aa797fb2a08880253a812f9f53a64b
Mándli Örs <mandliors@gmail.com>
2023-12-08 19:48:51 +0100

    refactoring

--- src/expression_parser/expression_parser.c
+++ src/expression_parser/expression_parser.c
@@ -1,37 +1,34 @@
 #include "expression_parser.h"
 
-Node* create_variable(char c)
+Node* create_variable(Variable variable)
 {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = VARIABLE;
-    node->data.variable = (Variable){c};
+    node->data.variable = variable;
     node->left = node->right = NULL;
     return node;
 }
-Node* create_number(int n)
+Node* create_number(Number number)
 {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = NUMBER;
-    node->data.number = (Number){n};
+    node->data.number = number;
     node->left = node->right = NULL;
     return node;
 }
-Node* create_function(const char* name, Func func)
+Node* create_function(Function function)
 {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = FUNCTION;
-    Function f;
-    strcpy(f.name, name);
-    f.func = func;
-    node->data.function = f;
+    node->data.function = function;
     node->left = node->right = NULL;
     return node;
 }
-Node* create_operator(char operator)
+Node* create_operator(Operator operator)
 {
     Node* node = (Node*)malloc(sizeof(Node));
     node->type = OPERATOR;
-    node->data.operator = (Operator){operator};
+    node->data.operator = operator;
     node->left = node->right = NULL;
     return node;
 }
@@ -40,6 +37,7 @@ Node* parse(char* expression)
 {
     NodeStack stack; stack.top = -1;
     char* token = strtok(expression, " ");
+    char* c;
     while (token != NULL)
     {
         if (strcmp(token, "x") == 0) //variable
@@ -47,9 +45,18 @@ Node* parse(char* expression)
             Node* node = create_variable('x');
             push(&stack, node);
         }
-        else if (strpbrk("+-*/^", token) != NULL) //operator
+        else if ((c = strpbrk("+-*/^", token)) != NULL) //operator
         {
-            Node* node = create_operator(token[0]);
+            Node* node;
+            switch(*c)
+            {
+                case '+': node = create_operator(add); break;
+                case '-': node = create_operator(subtract); break;
+                case '*': node = create_operator(multiply); break;
+                case '/': node = create_operator(divide); break;
+                case '^': node = create_operator(pow); break;
+                default: exit(1);
+            }
             Node* right = pop(&stack);
             Node* left = pop(&stack);
             node->left = left;
@@ -58,42 +65,42 @@ Node* parse(char* expression)
         }
         else if(strcmp(token, "sin") == 0) //function
         {
-            Node* node = create_function("sin", sin);
+            Node* node = create_function(sin);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
         }
         else if(strcmp(token, "cos") == 0)
         {
-            Node* node = create_function("cos", cos);
+            Node* node = create_function(cos);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
         }
         else if(strcmp(token, "tg") == 0)
         {
-            Node* node = create_function("tg", tan);
+            Node* node = create_function(tan);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
         }
         else if(strcmp(token, "ctg") == 0)
         {
-            Node* node = create_function("ctg", cot);
+            Node* node = create_function(cot);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
         }
         else if(strcmp(token, "ln") == 0)
         {
-            Node* node = create_function("ln", log);
+            Node* node = create_function(log);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
         }
         else if(strcmp(token, "exp") == 0)
         {
-            Node* node = create_function("exp", exp);
+            Node* node = create_function(exp);
             Node* left = pop(&stack);
             node->left = left;
             push(&stack, node);
@@ -115,21 +122,11 @@ double evaluate(Node* head, double x)
     if (head->type == VARIABLE)
         return x;
     else if (head->type == NUMBER)
-        return head->data.number.number;
+        return head->data.number;
     else if (head->type == OPERATOR)
-    {
-        switch (head->data.operator.operator)
-        {
-            case '+': return evaluate(head->left, x) + evaluate(head->right, x);
-            case '-': return evaluate(head->left, x) - evaluate(head->right, x);
-            case '*': return evaluate(head->left, x) * evaluate(head->right, x);
-            case '/': return evaluate(head->left, x) / evaluate(head->right, x);
-            case '^': return pow(evaluate(head->left, x), evaluate(head->right, x));        
-            default: exit(1);
-        }
-    }
-    else //FUNCTION
-        return head->data.function.func(x);
+        return head->data.operator(evaluate(head->left, x), evaluate(head->right, x));
+    else
+        return head->data.function(x);
 }
 
 void push(NodeStack* stack, Node* node)
@@ -143,4 +140,8 @@ Node* pop(NodeStack* stack)
     return stack->data[stack->top--];
 }
 
+double add(double a, double b) { return a + b; }
+double subtract(double a, double b) { return a - b; }
+double multiply(double a, double b) { return a * b; }
+double divide(double a, double b) { return a / b; }
 double cot(double x) { return 1.0 / tan(x); };
--- src/expression_parser/expression_parser.h
+++ src/expression_parser/expression_parser.h
@@ -1,3 +1,5 @@
+#pragma once
+
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
@@ -5,23 +7,10 @@
 
 #define STACK_SIZE 50
 
-typedef struct Variable {
-    char c;
-} Variable;
-
-typedef struct Number {
-    double number;
-} Number;
-
-typedef double (*Func)(double);
-typedef struct Function {
-    char name[4+1];
-    Func func;
-} Function;
-
-typedef struct Operator {
-    char operator;
-} Operator;
+typedef char Variable;
+typedef double Number;
+typedef double(*Function)(double);
+typedef double(*Operator)(double, double);
 
 typedef enum NodeType {
     VARIABLE, NUMBER, FUNCTION, OPERATOR
@@ -45,10 +34,10 @@ typedef struct NodeStack {
     int top;
 } NodeStack;
 
-Node* create_variable(char c);
-Node* create_number(int n);
-Node* create_function(const char* name, Func func);
-Node* create_operator(char operator);
+Node* create_variable(Variable variable);
+Node* create_number(Number number);
+Node* create_function(Function function);
+Node* create_operator(Operator operator);
 
 Node* parse(char* expression);
 double evaluate(Node* head, double x);
@@ -56,4 +45,8 @@ double evaluate(Node* head, double x);
 void push(NodeStack* stack, Node* node);
 Node* pop(NodeStack* stack);
 
+double add(double a, double b);
+double subtract(double a, double b);
+double multiply(double a, double b);
+double divide(double a, double b);
 double cot(double x);
